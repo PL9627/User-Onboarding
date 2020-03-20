af5033f31f4447d99a80a5a657b4a731f5e1d802
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-03-19 22:05:47 -0700

    got the needed info to render to browser

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import UserForm from './components/UserForm';
+
 
 function App() {
   return (
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,37 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
-import axios from "axios";
+import axios from 'axios';
+
 
 const UserForm = props => {
   const { touched, errors, status } = props;
 
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+      status && setUsers(status);
+  }, [status]);
+
   return (
-    <Form>
-      <label htmlFor="name">Name:</label>
-      <Field name="name" placeholder="Type your name here" />
-      {touched.name && errors.name ? (
-        <span className="error">{errors.name}</span>
-      ) : null}
-      ;<label htmlFor="email">Email:</label>
-      <Field name="email" placeholder="Type your email here" />
-      {touched.email && errors.email ? (
-        <span className="error">{errors.email}</span>
-      ) : null}
-      ;<label htmlFor="password">Password:</label>
-      <Field name="password" placeholder="Type password here" />
-      {touched.password && errors.password ? (
-        <span className="error">{errors.password}</span>
-      ) : null}
-      <label htmlFor="TOS">Please read and agree to the TOS</label>
-      <Field name="tos" type="checkbox" />
-      {touched.tos && errors.tos ? (
-        <span className="error">{errors.tos}</span>
-      ) : null}
-      <button type="submit" disabled={!props.isValid}>
-        Submit
-      </button>
-    </Form>
+    <div>
+        <Form>
+            <label htmlFor="name">Name:</label>
+            <Field name="name" placeholder="Type your name here" />
+            {touched.name && errors.name ? (
+                <span className="error">{errors.name}</span>
+            ) : null}
+            ;<label htmlFor="email">Email:</label>
+            <Field name="email" placeholder="Type your email here" />
+            {touched.email && errors.email ? (
+                <span className="error">{errors.email}</span>
+            ) : null}
+            ;<label htmlFor="password">Password:</label>
+            <Field name="password" placeholder="Type password here" />
+            {touched.password && errors.password ? (
+                <span className="error">{errors.password}</span>
+            ) : null}
+            <label htmlFor="TOS">Please read and agree to the TOS</label>
+            <Field name="tos" type="checkbox" />
+            {touched.tos && errors.tos ? (
+                <span className="error">{errors.tos}</span>
+            ) : null}
+            <button type="submit" disabled={!props.isValid}>
+                Submit
+            </button>
+        </Form>
+        {users.name && (
+            <ul key = {users.id}>
+                <li>Name: {users.name}</li>
+                <li>email: {users.email}</li>
+                <li>password: {users.password}</li>
+            </ul>
+        )}
+    </div>
   );
 };
 
@@ -54,11 +70,14 @@ export default withFormik({
       .required("A password is required"),
     terms: Yup.boolean().oneOf([true], 'Please agree to the TOS')
   }),
-  handleSubmit: (values, formikBag) => {
-      formikBag.props.addUser({
-          ...values, id: Date.now()
-      });
-      formikBag.setStatus('Submitting user form...');
-      formikBag.resetForm();
+  handleSubmit: (values, { resetForm, setStatus }) => {
+      //console.log(formikBag);
+      axios.post("https://reqres.in/api/users", values)
+      .then(response => {
+          console.log(response);
+          setStatus(response.data);
+          resetForm();
+      })
+      .catch(err => console.log(err.response))
   }
 })(UserForm);
