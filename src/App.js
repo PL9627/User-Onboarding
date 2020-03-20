4a4b1e5ce93301aa51e61dbe2de1af9eb8bada4d
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-03-19 17:21:26 -0700

    some necessary cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UserForm from './components/UserFrom';
+import UserForm from './components/UserForm';
 
 function App() {
   return (
