997db21ab97b67cc291f2935102f8c1ea9f9b229
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-03-19 17:47:42 -0700

    finished validationSchema and started on handleSubmit

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,48 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { withFormik, Form, Field } from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 const UserForm = props => {
-    const { touched, errors, status } = props;
+  const { touched, errors, status } = props;
 
-    return(
-        <Form>
-            <label htmlFor = 'name'>Name:</label>
-            <Field name ='name' placeholder = 'Type your name here'/>
-            {touched.name && errors.name ? (
-                <span className = 'error'>{errors.name}</span>
-            ) : null};
-            <label htmlFor = 'email'>Email:</label>
-            <Field name = 'email' placeholder = 'Type your email here'/>
-            {touched.email && errors.email ? (
-                <span className = 'error'>{errors.email}</span>
-            ) : null};
-            <label htmlFor = 'password'>Password:</label>
-            <Field name = 'password' placeholder = 'Type password here'/>
-            {touched.password && errors.password ? (
-                <span className = 'error'>{errors.password}</span>
-            ) : null}
-            <label htmlFor = 'TOS'>Please read and agree to the TOS</label>
-            <Field name = 'tos' type = 'checkbox'/>
-            {touched.tos && errors.tos ? (
-                <span className = 'error'>{errors.tos}</span>
-            ) : null}
-            <button type = 'submit' disabled = {!props.isValid}>Submit</button>
-        </Form>
-    );
+  return (
+    <Form>
+      <label htmlFor="name">Name:</label>
+      <Field name="name" placeholder="Type your name here" />
+      {touched.name && errors.name ? (
+        <span className="error">{errors.name}</span>
+      ) : null}
+      ;<label htmlFor="email">Email:</label>
+      <Field name="email" placeholder="Type your email here" />
+      {touched.email && errors.email ? (
+        <span className="error">{errors.email}</span>
+      ) : null}
+      ;<label htmlFor="password">Password:</label>
+      <Field name="password" placeholder="Type password here" />
+      {touched.password && errors.password ? (
+        <span className="error">{errors.password}</span>
+      ) : null}
+      <label htmlFor="TOS">Please read and agree to the TOS</label>
+      <Field name="tos" type="checkbox" />
+      {touched.tos && errors.tos ? (
+        <span className="error">{errors.tos}</span>
+      ) : null}
+      <button type="submit" disabled={!props.isValid}>
+        Submit
+      </button>
+    </Form>
+  );
 };
 
 export default withFormik({
-    mapPropsToValues: props => {
-        return {
-            name: props.name || "",
-            email: props.email || "",
-            password: props.password || "",
-            tos: props.tos || false
-        };
-    },
-    validationSchema: Yup.object().shape({
-        name: Yup.string().required('Name is a required field')
-    })
+  mapPropsToValues: props => {
+    return {
+      name: props.name || "",
+      email: props.email || "",
+      password: props.password || "",
+      tos: props.tos || false
+    };
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("Name is a required field"),
+    email: Yup.string()
+      .email("Must be a valid email address")
+      .required("Must include email address"),
+    password: Yup.string()
+      .min(8, "Passwords must be at least 8 characters long")
+      .required("A password is required"),
+    terms: Yup.boolean().oneOf([true], 'Please agree to the TOS')
+  }),
+  handleSubmit: (values, formikBag) => {
+      
+  }
 })(UserForm);
